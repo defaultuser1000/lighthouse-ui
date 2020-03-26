f544c6ba11b6e6c2e8bdf444546678f8d62658ae
Andrey Zakrzhevskiy <and.zak.one@gmail.com>
2020-03-27 00:34:19 +0300

    Changed node_env strategy

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -25,7 +25,7 @@ const prod = {
         API_URL: "https://lighthouse-back-production.herokuapp.com"
     }
 };
-export const config = process.env.STAND === "production" ? prod
-    : process.env.STAND === "development" ? dev
-        : process.env.STAND === "staging" ? staging
+export const config = process.env.NODE_ENV === "production" ? prod
+    : process.env.NODE_ENV === "development" ? dev
+        : process.env.NODE_ENV === "staging" ? staging
             : dev;
