d80f5c275cc21e09812d418b1e4ea9980379a6a9
Andrey Zakrzhevskiy <and.zak.one@gmail.com>
2020-10-03 21:25:05 +0300

    Trying to start client on heroku

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-    app.use(proxy('/**', { target: 'https://lighthouse-back-dev.herokuapp.com' }));
-    // app.use(proxy('/otherApi/**', { target: 'http://localhost:5000' }));
-};
